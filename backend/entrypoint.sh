b3464046f6ae8726adc2a99bd960246eaf85bca6
Joshua Jadge Garcia <joshuajadge.garcia@cit.edu>
2024-10-28 02:30:22 +0800

    Minor changes

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,9 +2,9 @@
 pip3 install -r /requirements.txt
 
 # May as well do this too, while we're here.
-python manage.py makemigrations
-python manage.py migrate
-python manage.py loaddata criteria_fixture.json
+python /backend/manage.py makemigrations
+python /backend/manage.py migrate
+python /backend/manage.py loaddata criteria_fixture.json
 
 # execs $CMD
 exec "$@"
