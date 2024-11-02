3f5c1bb5d3458cedd8ea2dfeddcbe59fc8a50571
Joshua Jadge Garcia <joshuajadge.garcia@cit.edu>
2024-11-02 17:48:16 +0800

    Updated

--- frontend/src/services/config.js
+++ frontend/src/services/config.js
@@ -2,7 +2,7 @@ const apiHost = 'wild-forge-production.up.railway.app';
 const apiPort = '8000';
 
 const apiConfig = {
-  API_URL: `http://${apiHost}:${apiPort}`,
+  API_URL: `https://${apiHost}:${apiPort}`,
 };
 
 export default apiConfig;
