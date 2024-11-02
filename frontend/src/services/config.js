35951acb173ac1ad8ab13dcc17c5e049c4338df2
Joshua Jadge Garcia <joshuajadge.garcia@cit.edu>
2024-11-02 17:42:48 +0800

    Testing

--- frontend/src/services/config.js
+++ frontend/src/services/config.js
@@ -1,8 +1,8 @@
-const apiHost = process.env.REACT_APP_API_HOST ?? 'wild-forge-production.up.railway.app';
-const apiPort = process.env.REACT_APP_API_PORT;  // This will be undefined in production
+const apiHost = 'wild-forge-production.up.railway.app';
+const apiPort = '8000';
 
 const apiConfig = {
-  API_URL: 'https://wild-forge-production.up.railway.app',
+  API_URL: `http://${apiHost}:${apiPort}`,
 };
 
 export default apiConfig;
